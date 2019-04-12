256d96e6e2418a3aa26cbb0a6b370f80292a997d
YuQiao0303 <691761071@qq.com>
2019-04-12 19:42:20 +0800

    modify key io into Version 2.0

--- Projects/ble/SimpleUart2Uart-factory-V1.41/Source/myKey.C
+++ Projects/ble/SimpleUart2Uart-factory-V1.41/Source/myKey.C
@@ -1,8 +1,9 @@
 #include <ioCC2540.h>
 #include "myKey.H"
-//0 in 
+//0 in 把按键所在位置为0
 //1 out
 /*
+v1.0
 #define KEY1 P1_3
 #define KEY2 P0_6
 #define KEY3 P1_4
@@ -22,6 +23,22 @@
 #define KEY_DOWN 0
 #define KEY_UP 1
 #define DEFULT_UP 1
+*/
+
+/*
+//---------v2.0--------
+#define KEY1 P0_0  //s5
+#define KEY2 P0_6  //s7
+#define KEY3 P0_1  //s6
+#define KEY4 P0_7  //s8
+#define KEY5 P1_7  //s1
+#define KEY6 P1_5  //s3
+#define KEY7 P1_6  //s2
+#define KEY8 P1_4  //s4
+//-----------------
+
+0   0011 1100   3c
+1   0000 1111   0f
 */
 uchar currentKeys[3];  //电平
 uchar lastKeys[3] ={0,0,0};
@@ -43,12 +60,15 @@ void myKey_init()
     android_ready = false;
 
     //重新配置IO口方向
-      P0DIR &= 0xa9; 
-    
-//      P0DIR &= 0xad; 
-      P1DIR &= 0xe5; 
-      P2DIR &= 0xfe; 
-      
+    //v1.0
+//      P0DIR &= 0xa9; 
+//    
+////      P0DIR &= 0xad; 
+//      P1DIR &= 0xe5; 
+//      P2DIR &= 0xfe; 
+      //v2.0
+        P0DIR &= 0x3c;
+        P1DIR &= 0x0f;
       if(DEMO)
       {
           /*默认松开*/
